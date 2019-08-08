94a45a34f0e45ec3f36129e2c71c9a4dd251fbdf
Ojas Mehta <ojasmehta189@gmail.com>
2019-08-08 19:35:48 +0530

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','cam.ac.uk','mac.com','uct.ac.za','unicon.net','berkeley.edu','gmail.com','ucdavis.edu','stanford.edu'],
+['2005',57,12,7,12,14,6,12,10,11,4],
+['2006',1042,496,442,373,343,318,306,272,251,193]
+];
